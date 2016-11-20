5ffb2dc3fb989c660548a65e1664116ec7cf3d4e
Hennemuth <wrh@bu.edu>
2016-11-20 16:20:33 -0500

    Finishing kunagi.sh for virtualbox

--- kunagi/kunagi.sh
+++ kunagi/kunagi.sh
@@ -1,6 +1,9 @@
+# Default user = 'admin', password = 'geheim'
+
 if [ -z "$(docker ps -a --filter name=kunagi | grep kunagi)" ] ; then
    docker run \
       -d \
+      --privileged \
       -p 8484:8080 \
       --name kunagi \
       -v /tmp/kunagi-data:/usr/local/tomcat/webapps/kunagi-data \
