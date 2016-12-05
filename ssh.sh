72f6052b5478d1fa39b2be28ecdea6713efa1ac2
Hennemuth <wrh@bu.edu>
2016-12-05 09:23:03 -0500

    removing ssh.sh

--- ssh.sh
+++
@@ -1,5 +0,0 @@
-eval `ssh-agent -s`
-ssh-add C:/Users/wrh/.ssh/github_id_rsa
-if [ -z "$(git remote | grep github)" ] ; then
-   git remote add github git@github.com:whennemuth/agile-scrum-board-software.git
-fi
